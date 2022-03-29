fd427b6012e56c8eb2023e7a21ae7f6eb967a9e9
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-03-28 21:00:55 -0400

    Update inital testing.py

--- inital testing.py
+++ inital testing.py
@@ -4,7 +4,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #%% reading in data 
-
-df = pd.read_csv (r'C:\Users\pavan\Desktop\Git\6103 project\netflix_titles.csv')   
-
+df = pd.read_csv (r'C:\Users\pavan\Desktop\Git\6103 project\airline_data.csv') 
 
