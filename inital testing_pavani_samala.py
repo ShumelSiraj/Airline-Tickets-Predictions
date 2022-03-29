b38a32fadf85401d41e72088ffa77985f52d0d5e
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-03-28 21:07:23 -0400

    Create inital testing_pavani_samala.py

---
+++ inital testing_pavani_samala.py
@@ -0,0 +1,8 @@
+#%% importing in libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%% reading in data 
+df = pd.read_csv (r'C:\Users\pavan\Desktop\Git\6103 project\airline_data.csv') 
+
