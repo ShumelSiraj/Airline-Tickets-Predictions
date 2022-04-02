8f539fb37f900140532def4848c073dc1c589cf5
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-02 15:02:47 -0400

    Create inital testing_ps2.py

---
+++ inital testing_ps2.py
@@ -0,0 +1,63 @@
+#%%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %%
+df= pd.read_csv("Clean_Dataset.csv")
+del df['Unnamed: 0']
+
+#%%[markdown]
+# Changed the "class" column values to numeric :   <br>
+#
+# "Economy" = 0   <br>
+# "Business" = 1
+
+# %%
+df['class'].replace(['Economy', 'Business'], [0, 1], inplace=True)
+
+#%%[markdown]
+# Changed the "arrival_time" and "departure_time" column values to numeric :<br>
+#
+# 'Early_Morning' = 0   <br>
+# 'Morning' = 1         <br>
+# 'Afternoon' = 2       <br>
+# 'Evening' = 3         <br>
+# 'Night' = 4           
+
+# %%
+df['arrival_time'].replace(['Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'], [0, 1, 2, 3, 4, 5], inplace=True)
+df['departure_time'].replace(['Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'], [0, 1, 2, 3, 4, 5], inplace=True)
+
+#%%[markdown]
+# Changed the "source_city" and "destination_city" column values to numeric :  <br>    
+#
+# 'Mumbai' = 0     <br>
+# 'Delhi' = 1      <br>
+# 'Bangalore' = 2  <br> 
+# 'Kolkata' = 3    <br> 
+# 'Hyderabad' = 4  <br>  
+# 'Chennai' = 5    <br>  
+
+#%%
+df['source_city'].replace(["Mumbai", "Delhi", "Bangalore", "Kolkata", "Hyderabad", "Chennai"], [0, 1, 2, 3, 4, 5], inplace= True)
+df['destination_city'].replace(["Mumbai", "Delhi", "Bangalore", "Kolkata", "Hyderabad", "Chennai"], [0, 1, 2, 3, 4, 5], inplace= True)
+#%%[markdown]
+# Changed the "stops" column values to numeric : <br>
+#
+# "zero" = 0   <br>
+# "one" = 1    <br>
+# "two_or_more" = 2
+
+#%%
+df['stops'].replace(['zero', 'one', 'two_or_more'], [0, 1, 2], inplace=True)
+# %%
+corr = df.corr()
+ax1 = sns.heatmap(corr, cbar=0, linewidths=2,vmax=1, vmin=0, square=True, cmap='Blues')
+plt.title("correlation of all the variables")
+plt.show()
+# %%
+
+# %%
+df
+
