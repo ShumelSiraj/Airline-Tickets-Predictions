709dddf432aced36aeef58452ee8ec43462463a8
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-02 22:40:59 -0400

    Update inital testing_ps2.py

--- inital testing_ps2.py
+++ inital testing_ps2.py
@@ -99,7 +99,44 @@ spicejet=spicejet.pivot_table(index='source_city', columns ='destination_city', 
 sns.heatmap(spicejet)
 
 # %%
+fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+air_india=df[df['airline']=="Air_India"]
+sns.regplot( ax=ax1, x="duration", y="price", data=air_india[air_india['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=air_india[air_india['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=air_india[air_india['stops']==2])
 
+fig2, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+spicejet=df[df['airline']=='SpiceJet']
+sns.regplot( ax=ax1, x="duration", y="price", data=spicejet[spicejet['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=spicejet[spicejet['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=spicejet[spicejet['stops']==2])
+
+fig3, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+indigo=df[df['airline']=='Indigo']
+sns.regplot( ax=ax1, x="duration", y="price", data=indigo[indigo['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=indigo[indigo['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=indigo[indigo['stops']==2])
+
+
+fig4, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+gofirst=df[df['airline']=='GO_FIRST']
+sns.regplot( ax=ax1, x="duration", y="price", data=gofirst[gofirst['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=gofirst[gofirst['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=gofirst[gofirst['stops']==2])
+
+fig5, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+vistara=df[df['airline']=='Vistara']
+sns.regplot( ax=ax1, x="duration", y="price", data=vistara[vistara['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=vistara[vistara['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=vistara[vistara['stops']==2])
+
+fig6, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(40,20))
+airasia=df[df['airline']=='AirAsia']
+sns.regplot( ax=ax1, x="duration", y="price", data=airasia[airasia['stops']==0])
+sns.regplot( ax=ax2, x="duration", y="price", data=airasia[airasia['stops']==1])
+sns.regplot( ax=ax3, x="duration", y="price", data=airasia[airasia['stops']==2])
+
+plt.show()
 # %%
 spicejet=df[df['airline']=='SpiceJet']
 spicejet=spicejet[['price','duration','stops']]
