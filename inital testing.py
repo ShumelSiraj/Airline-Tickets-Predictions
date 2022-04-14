d74550e9bb6731a4b242a78a83d3bcb907940050
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-14 08:13:35 -0400

    Update inital testing.py

--- inital testing.py
+++ inital testing.py
@@ -293,6 +293,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error as MSE
+from sklearn import tree
 #%%
 x_Air=econ[['stops', 'duration', 'days_left']]
 y_Air=econ['price']
@@ -312,9 +313,11 @@ xtrain, xtest, ytrain, ytest = train_test_split(x_Air, y_Air, test_size=0.2,rand
 
 air_tree = DecisionTreeRegressor(max_depth=4, min_samples_leaf=0.1,random_state=1)
 
-air_tree.fit(xtrain, ytrain)
+clf=air_tree.fit(xtrain, ytrain)
 price_pred = air_tree.predict(xtest)
 
 mse = MSE(ytest, price_pred)
 print(mse** (.5))
+# %%
+tree.plot_tree(clf)
 # %%
