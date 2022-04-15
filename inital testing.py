71f65557bec9e96c9cdb8317058cede52c941a97
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-14 20:02:21 -0400

    do it

--- inital testing.py
+++ inital testing.py
@@ -83,7 +83,6 @@ plt.title("correlation of all the variables")
 plt.show()
 # %%
 
-<<<<<<< HEAD
 # %%
 sns.pairplot(df)
 
@@ -117,170 +116,9 @@ buz=df[df['class']==1]
 # Air_India
 fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20), sharey=True)
 air_india=econ[econ['airline']=="Air_India"]
-=======
-#%% subset of economy and business data 
-econ=df[df['class']==0]
-econ_source=df_source[df_source['class']=='Economy']
-
-buz=df[df['class']==0]
-buz_source=df_source[df_source['class']=='Business']
-# %% scatterplots for each airline for different number of stops
-
-# Economy
-fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20))
-plt.xlim(0, 55)
->>>>>>> main
 fig1.suptitle("Airline: Air_India", fontsize=60)
 ax1=sns.regplot( ax=ax1, x="duration", y="price", data=econ[econ['stops']==0],scatter_kws={"color": "black"}, line_kws={"color": "red"})
 ax1.set_title("0 stops", fontsize=30)
-<<<<<<< HEAD
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=air_india[air_india['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax2.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=air_india[air_india['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-#%%
-
-econ=df[df['class']==0]
-buz=df[df['class']==1]
-# Air_India
-fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20), sharey=True)
-air_india=buz[buz['airline']=="Air_India"]
-fig1.suptitle("Airline: Air_India", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=air_india[air_india['stops']==0],scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=air_india[air_india['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax2.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=air_india[air_india['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-#%%
-# SpiceJet
-fig2, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20),sharey=True)
-spicejet=df[df['airline']=='SpiceJet']
-fig2.suptitle("Airline: SpiceJet", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=spicejet[spicejet['stops']==0],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=spicejet[spicejet['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax1.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=spicejet[spicejet['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-
-# Indigo
-fig3, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20),sharey=True)
-indigo=df[df['airline']=='Indigo']
-fig3.suptitle("Airline: Indigo", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=indigo[indigo['stops']==0],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=indigo[indigo['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax1.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=indigo[indigo['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax1.set_xlim(0,55)
-
-# GO_FIRST
-fig4, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20),sharey=True)
-gofirst=df[df['airline']=='GO_FIRST']
-fig4.suptitle("Airline: GO_FIRST", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=gofirst[gofirst['stops']==0],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=gofirst[gofirst['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax2.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=gofirst[gofirst['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-
-# Vistara
-fig5, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20),sharey=True)
-vistara=df[df['airline']=='Vistara']
-fig5.suptitle("Airline: Vistara", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=vistara[vistara['stops']==0],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=vistara[vistara['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax2.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=vistara[vistara['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-
-# AirAsia
-fig6, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20),sharey=True)
-airasia=df[df['airline']=='AirAsia']
-fig6.suptitle("Airline: AirAsia", fontsize=60)
-ax1=sns.regplot( ax=ax1, x="duration", y="price", data=airasia[airasia['stops']==0],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax1.set_title("0 stops", fontsize=30)
-ax1.set_xlim(0,55)
-ax2=sns.regplot( ax=ax2, x="duration", y="price", data=airasia[airasia['stops']==1],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax2.set_title("1 stop", fontsize=30)
-ax2.set_xlim(0,55)
-ax3=sns.regplot( ax=ax3, x="duration", y="price", data=airasia[airasia['stops']==2],
-scatter_kws={"color": "black"}, line_kws={"color": "red"})
-ax3.set_title("2 or more stops", fontsize=30)
-ax3.set_xlim(0,55)
-#%% linear regression AirAsia
-model_AirAsia_1 = ols(formula='price ~ duration', data=airasia)
-model_AirAsia_1_Fit = model_AirAsia_1.fit()
-print( model_AirAsia_1_Fit.summary())
-
-model_AirAsia_2 = ols(formula='price ~ duration * stops', data=airasia)
-model_AirAsia_2_Fit = model_AirAsia_2.fit()
-print( model_AirAsia_2_Fit.summary())
-
-model_AirAsia_3 = ols(formula='price ~ duration + stops + days_left', data=airasia)
-model_AirAsia_3_Fit = model_AirAsia_3.fit()
-print( model_AirAsia_3_Fit.summary())
-# %%
-airasia2=df_source[df_source['airline']=='AirAsia']
-model_AirAsia_1 = ols(formula='price ~ duration', data=airasia2)
-model_AirAsia_1_Fit = model_AirAsia_1.fit()
-print( model_AirAsia_1_Fit.summary())
-
-model_AirAsia_2 = ols(formula='price ~ duration * stops', data=airasia2)
-model_AirAsia_2_Fit = model_AirAsia_2.fit()
-print( model_AirAsia_2_Fit.summary())
-
-model_AirAsia_3 = ols(formula='price ~ duration * stops * Coming_up', data=airasia2)
-model_AirAsia_3_Fit = model_AirAsia_3.fit()
-print( model_AirAsia_3_Fit.summary())
-# %% linear model for economy class tickets
-econ=df[df['class']==0]
-econ_source=df_source[df_source['class']=='Economy']
-
-model_econ_1 = ols(formula='price ~ duration', data=econ)
-model_econ_1_Fit = model_econ_1.fit()
-print(model_econ_1_Fit.summary())
-
-=======
 ax1.set_ylim(1, 40000)
 ax2=sns.regplot( ax=ax2, x="duration", y="price", data=econ[econ['stops']==1],
 scatter_kws={"color": "black"}, line_kws={"color": "red"})
@@ -300,7 +138,6 @@ model_econ_1 = ols(formula='price ~ duration', data=econ)
 model_econ_1_Fit = model_econ_1.fit()
 print(model_econ_1_Fit.summary())
 
->>>>>>> main
 model_econ_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', data=econ_source)
 model_econ_2_Fit = model_econ_2.fit()
 print(model_econ_2_Fit.summary())
@@ -309,15 +146,9 @@ model_econ_3 = ols(formula='price ~ duration * stops * Coming_up', data=econ_sou
 model_econ_3_Fit = model_econ_3.fit()
 print(model_econ_3_Fit.summary())
 
-<<<<<<< HEAD
 #%% linear model for business class tickets
 buz=df[df['class']==0]
 buz_source=df_source[df_source['class']=='Business']
-=======
-
-
-#%% linear model for business class tickets
->>>>>>> main
 
 model_buz_1 = ols(formula='price ~ duration', data=buz)
 model_buz_1_Fit = model_buz_1.fit()
@@ -326,7 +157,6 @@ print(model_buz_1_Fit.summary())
 model_buz_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', data=buz_source)
 model_buz_2_Fit = model_buz_2.fit()
 print(model_buz_2_Fit.summary())
-<<<<<<< HEAD
 
 model_buz_3 = ols(formula='price ~ duration * stops * Coming_up', data=buz_source)
 model_buz_3_Fit = model_buz_3.fit()
@@ -378,10 +208,4 @@ print(MSE_CV)
 plt.savefig('out.pdf')
 # %%
 #tree.export_graphviz(air_tree,out_file="tree.dot",filled = True)
-=======
-
-model_buz_3 = ols(formula='price ~ duration * stops * Coming_up', data=buz_source)
-model_buz_3_Fit = model_buz_3.fit()
-print(model_buz_3_Fit.summary())
->>>>>>> main
 # %%
