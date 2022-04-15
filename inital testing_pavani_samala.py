7dcc1e187b953df67caf6ae21ea2a6dced11c041
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-14 20:05:01 -0400

    Update inital testing_pavani_samala.py

--- inital testing_pavani_samala.py
+++ inital testing_pavani_samala.py
@@ -1,31 +1,139 @@
-#%% importing in libraries
+#%%
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-#%% reading in data 
-df = pd.read_csv (r'C:\Users\pavan\Desktop\Git\6103 project\airline_data.csv') 
+import matplotlib.pyplot as plt
+from statsmodels.formula.api import ols
+# %%
+df_source= pd.read_csv("Clean_Dataset.csv")
+del df_source['Unnamed: 0']
+
+coming_up=[]
+for value in df_source["days_left"]:
+    if 0 <= value <= 7:
+        coming_up.append("Very Soon")
+    if 8 <= value <= 14:
+        coming_up.append("Soon")
+    if 15 <= value <= 35:
+        coming_up.append("Far Away")
+    if value <= 36:
+        coming_up.append("Very Far Away")
+    else:
+        coming_up.append("NA")   
+df_source['Coming_up'] = pd.Series(coming_up)   
+print(df_source)
+
+
 #%%
-fig, axs = plt.subplots(2) 
-fig.set_size_inches(10,10)
+df= pd.read_csv("Clean_Dataset.csv")
+del df['Unnamed: 0']
 
-axs[0].plot(df['departure_time'],  df['price'],'o')
-axs[1].plot(df['stops'],  df['price'],'o')
-# %%
-fig, axes = plt.subplots(1,2,figsize=(20,15))
-sns.stripplot(ax=axes[0], data=df, x='class',y='price', dodge='true',hue='departure_time', jitter=.5)
-
-sns.stripplot(ax=axes[1], data=df, x='class',y='price', dodge='true',hue='source_city', jitter=.5)
+#%%[markdown]
+# Changed the "class" column values to numeric :   <br>
+#
+# "Economy" = 0   <br>
+# "Business" = 1
 
 # %%
-sc1=df[df['source_city']=='Dehli']
-sc2=df[df['source_city']=='Mumbai']
-sc3=df[df['source_city']=='Bangalore']
-sc4=df[df['source_city']=='Kolkata']
-sc5=df[df['source_city']=='Hyderabad']
-sc6=df[df['source_city']=='Chennai']
-data = [ sc1['price'], sc2['price'], sc3['price'], sc4['price'],sc5['price'],sc6['price'] ]
-plt.boxplot(data)
+df['class'].replace(['Economy', 'Business'], [0, 1], inplace=True)
+
+#%%[markdown]
+# Changed the "arrival_time" and "departure_time" column values to numeric :<br>
+#
+# 'Early_Morning' = 0   <br>
+# 'Morning' = 1         <br>
+# 'Afternoon' = 2       <br>
+# 'Evening' = 3         <br>
+# 'Night' = 4           
+
 # %%
-df.pivot(index="airline", columns="destination_city", values="price")
+df['arrival_time'].replace(['Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'], [0, 1, 2, 3, 4, 5], inplace=True)
+df['departure_time'].replace(['Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'], [0, 1, 2, 3, 4, 5], inplace=True)
+
+#%%[markdown]
+# Changed the "source_city" and "destination_city" column values to numeric :  <br>    
+#
+# 'Mumbai' = 0     <br>
+# 'Delhi' = 1      <br>
+# 'Bangalore' = 2  <br> 
+# 'Kolkata' = 3    <br> 
+# 'Hyderabad' = 4  <br>  
+# 'Chennai' = 5    <br>  
+
+#%%
+df['source_city'].replace(["Mumbai", "Delhi", "Bangalore", "Kolkata", "Hyderabad", "Chennai"], [0, 1, 2, 3, 4, 5], inplace= True)
+df['destination_city'].replace(["Mumbai", "Delhi", "Bangalore", "Kolkata", "Hyderabad", "Chennai"], [0, 1, 2, 3, 4, 5], inplace= True)
+#%%[markdown]
+# Changed the "stops" column values to numeric : <br>
+#
+# "zero" = 0   <br>
+# "one" = 1    <br>
+# "two_or_more" = 2
+
+#%%
+df['stops'].replace(['zero', 'one', 'two_or_more'], [0, 1, 2], inplace=True)
+# %%
+corr = df.corr()
+ax1 = sns.heatmap(corr, cbar=0, linewidths=2,vmax=1, vmin=0, square=True, cmap='Blues', annot=True,annot_kws = {'size':5})
+plt.title("correlation of all the variables")
+plt.show()
+# %%
+df
+
+#%% subset of economy and business data 
+econ=df[df['class']==0]
+econ_source=df_source[df_source['class']=='Economy']
+
+buz=df[df['class']==0]
+buz_source=df_source[df_source['class']=='Business']
+# %% scatterplots for each airline for different number of stops
+
+# Economy
+fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20))
+plt.xlim(0, 55)
+fig1.suptitle("Airline: Air_India", fontsize=60)
+ax1=sns.regplot( ax=ax1, x="duration", y="price", data=econ[econ['stops']==0],scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax1.set_title("0 stops", fontsize=30)
+ax1.set_ylim(1, 40000)
+ax2=sns.regplot( ax=ax2, x="duration", y="price", data=econ[econ['stops']==1],
+scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax2.set_title("1 stop", fontsize=30)
+ax2.set_ylim(1, 40000)
+ax3=sns.regplot( ax=ax3, x="duration", y="price", data=econ[econ['stops']==2],
+scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax3.set_title("2 or more stops", fontsize=30)
+ax3.set_ylim(1, 40000)
+
+# Business
+
+
+# %% linear model for economy class tickets
+
+model_econ_1 = ols(formula='price ~ duration', data=econ)
+model_econ_1_Fit = model_econ_1.fit()
+print(model_econ_1_Fit.summary())
+
+model_econ_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', data=econ_source)
+model_econ_2_Fit = model_econ_2.fit()
+print(model_econ_2_Fit.summary())
+
+model_econ_3 = ols(formula='price ~ duration * stops * Coming_up', data=econ_source)
+model_econ_3_Fit = model_econ_3.fit()
+print(model_econ_3_Fit.summary())
+
+
+
+#%% linear model for business class tickets
+
+model_buz_1 = ols(formula='price ~ duration', data=buz)
+model_buz_1_Fit = model_buz_1.fit()
+print(model_buz_1_Fit.summary())
+
+model_buz_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', data=buz_source)
+model_buz_2_Fit = model_buz_2.fit()
+print(model_buz_2_Fit.summary())
+
+model_buz_3 = ols(formula='price ~ duration * stops * Coming_up', data=buz_source)
+model_buz_3_Fit = model_buz_3.fit()
+print(model_buz_3_Fit.summary())
 # %%
