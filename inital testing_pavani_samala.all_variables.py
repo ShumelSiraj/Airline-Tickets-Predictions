d45810bfe0ad574b4406b4da44eafdc82fb052bf
PavaniSamala <65415527+PavaniSamala@users.noreply.github.com>
2022-04-16 16:58:08 -0400

    final

--- inital testing_pavani_samala.all_variables.py
+++ inital testing_pavani_samala.all_variables.py
@@ -117,7 +117,7 @@ model_econ_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', da
 model_econ_2_Fit = model_econ_2.fit()
 print(model_econ_2_Fit.summary())
 
-model_econ_3 = ols(formula='price ~ duration * stops * days_left', data=econ)
+model_econ_3 = ols(formula='price ~ I(duration*duration) + (duration*stops) + days_left', data=econ)
 model_econ_3_Fit = model_econ_3.fit()
 print(model_econ_3_Fit.summary())
 
@@ -133,7 +133,7 @@ model_buz_2 = ols(formula='price ~ I(duration*duration) + (duration*stops)', dat
 model_buz_2_Fit = model_buz_2.fit()
 print(model_buz_2_Fit.summary())
 
-model_buz_3 = ols(formula='price ~ duration * stops * days_left', data=buz)
+model_buz_3 = ols(formula='price ~ I(duration*duration) + (duration*stops) + days_left', data=buz)
 model_buz_3_Fit = model_buz_3.fit()
 print(model_buz_3_Fit.summary())
 # %%
@@ -149,7 +149,7 @@ x_Air_econ=econ[['stops', 'duration', 'days_left']]
 y_Air_econ=econ['price']
 xtrain_econ, xtest_econ, ytrain_econ, ytest_econ = train_test_split(x_Air_econ, y_Air_econ, test_size=0.2,random_state=1)
 
-air_tree_econ = DecisionTreeRegressor(max_depth=8, min_samples_leaf=0.1,random_state=44)
+air_tree_econ = DecisionTreeRegressor(max_depth=8, min_samples_leaf=1,random_state=50)
 
 plane=air_tree_econ.fit(xtrain_econ, ytrain_econ)
 price_pred_econ = air_tree_econ.predict(xtest_econ)
@@ -182,7 +182,7 @@ x_Air_buz=buz[['stops', 'duration', 'days_left']]
 y_Air_buz=buz['price']
 xtrain_buz, xtest_buz, ytrain_buz, ytest_buz = train_test_split(x_Air_buz, y_Air_buz, test_size=0.2,random_state=1)
 
-air_tree_buz = DecisionTreeRegressor(max_depth=10, min_samples_leaf=1,random_state=44)
+air_tree_buz = DecisionTreeRegressor(max_depth=8, min_samples_leaf=1,random_state=50)
 
 plane=air_tree_buz.fit(xtrain_buz, ytrain_buz)
 price_pred_buz = air_tree_buz.predict(xtest_buz)
@@ -212,7 +212,7 @@ plt.legend()
 plt.show()
 # %%
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=25)
 knn.fit(xtrain_econ,ytrain_econ)
 knn_price_pred_econ = knn.predict(xtest_econ)
 print(knn.score(xtest_econ,ytest_econ))
@@ -220,7 +220,7 @@ print('R2 Value:',r2_score(ytest_econ, knn_price_pred_econ))
 
 # %%
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=25)
 knn.fit(xtrain_buz,ytrain_buz)
 knn_price_pred_econ = knn.predict(xtest_buz)
 print(knn.score(xtest_buz,ytest_buz))
@@ -232,35 +232,36 @@ model_econ_1 = ols(formula='price ~ C(stops)', data=econ)
 model_econ_1_Fit = model_econ_1.fit()
 print(model_econ_1_Fit.summary())
 
-model_econ_2 = ols(formula='price ~ C(stops) * days_left* C(source_city)', data=econ)
+model_econ_2 = ols(formula='price ~ C(stops) * days_left * C(source_city)', data=econ)
 model_econ_2_Fit = model_econ_2.fit()
 print(model_econ_2_Fit.summary())
 
-model_econ_3 = ols(formula='price ~  C(stops) * days_left * C(source_city) * C(destination_city)', data=econ)
+model_econ_3 = ols(formula='price ~  C(stops) + days_left + duration + C(source_city) + C(destination_city)', data=econ)
 model_econ_3_Fit = model_econ_3.fit()
 print(model_econ_3_Fit.summary())
 
 
 
-#%% linear model for business class tickets
+#%% linear model for business class tickets (set 2)
 
 model_buz_1 = ols(formula='price ~ C(stops)', data=buz)
 model_buz_1_Fit = model_buz_1.fit()
 print(model_buz_1_Fit.summary())
 
-model_buz_2 = ols(formula='price ~ C(stops) * C(source_city)', data=buz_source)
+model_buz_2 = ols(formula='price ~ C(stops) * days_left * C(source_city)', data=buz)
 model_buz_2_Fit = model_buz_2.fit()
 print(model_buz_2_Fit.summary())
 
-model_buz_3 = ols(formula='price ~ C(stops) * C(source_city) * C(destination_city)', data=buz_source)
+model_buz_3 = ols(formula='price ~  C(stops) + days_left + duration + C(source_city) + C(destination_city)', data=buz)
 model_buz_3_Fit = model_buz_3.fit()
 print(model_buz_3_Fit.summary())
-# %% Regression Tree for Economy Class
-x_Air_econ=econ[['stops', 'source_city', 'destination_city']]
+
+# %% Regression Tree for Economy Class (set 2)
+x_Air_econ=econ[['stops', 'duration', 'days_left', 'source_city', 'destination_city']]
 y_Air_econ=econ['price']
 xtrain_econ, xtest_econ, ytrain_econ, ytest_econ = train_test_split(x_Air_econ, y_Air_econ, test_size=0.2,random_state=1)
 
-air_tree_econ = DecisionTreeRegressor(max_depth=8, min_samples_leaf=0.1,random_state=44)
+air_tree_econ = DecisionTreeRegressor(max_depth=8, min_samples_leaf=1,random_state=50)
 
 plane=air_tree_econ.fit(xtrain_econ, ytrain_econ)
 price_pred_econ = air_tree_econ.predict(xtest_econ)
@@ -278,19 +279,27 @@ print(MSE_CV)
 
 tree.export_graphviz(air_tree_econ,out_file="Regression_Tree_Econ_2.dot",filled = True, feature_names=fn)
 #%%
-ECON=sns.scatterplot(ytest_econ,price_pred_econ)
+number_of_observations=50
+x_ax = range(len(ytest_econ[:number_of_observations]))
+plt.plot(x_ax, ytest_econ[:number_of_observations], label="Actual")
+plt.plot(x_ax, price_pred_econ[:number_of_observations], label="Predicted")
+plt.title("Actual Flight Prices vs Predicted Prices (Economy)")
+plt.xlabel('Observation Number')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
 
-# %% Regression Tree for Business Class
-x_Air_buz=buz[['stops', 'source_city', 'destination_city']]
+# %% Regression Tree for Business Class (set 2)
+x_Air_buz=buz[['stops', 'duration', 'days_left', 'source_city', 'destination_city']]
 y_Air_buz=buz['price']
 xtrain_buz, xtest_buz, ytrain_buz, ytest_buz = train_test_split(x_Air_buz, y_Air_buz, test_size=0.2,random_state=1)
 
-air_tree_buz = DecisionTreeRegressor(max_depth=8, min_samples_leaf=0.1,random_state=44)
+air_tree_buz = DecisionTreeRegressor(max_depth=8, min_samples_leaf=1,random_state=50)
 
 plane=air_tree_buz.fit(xtrain_buz, ytrain_buz)
 price_pred_buz = air_tree_buz.predict(xtest_buz)
 
-fn=['stops', 'source_city', 'destination_city']
+fn=['stops', 'duration', 'days_left', 'source_city', 'destination_city']
 tree.plot_tree(plane,feature_names=fn)
 
 print("The mean square error is:", MSE(ytest_buz, price_pred_buz))
@@ -303,10 +312,18 @@ print(MSE_CV)
 
 tree.export_graphviz(air_tree_buz,out_file="Regression_Tree_Buz2.dot",filled = True, feature_names=fn)
 # %%
-BUZ=sns.scatterplot(ytest_buz,price_pred_buz)
+number_of_observations=50
+x_ax = range(len(ytest_buz[:number_of_observations]))
+plt.plot(x_ax, ytest_buz[:number_of_observations], label="Actual")
+plt.plot(x_ax, price_pred_buz[:number_of_observations], label="Predicted")
+plt.title("Actual Flight Prices vs Predicted Prices (Business)")
+plt.xlabel('Observation Number')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
 # %%
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=25)
 knn.fit(xtrain_econ,ytrain_econ)
 knn_price_pred_econ = knn.predict(xtest_econ)
 print(knn.score(xtest_econ,ytest_econ))
@@ -314,7 +331,7 @@ print('R2 Value:',r2_score(ytest_econ, knn_price_pred_econ))
 
 # %%
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=25)
 knn.fit(xtrain_buz,ytrain_buz)
 knn_price_pred_econ = knn.predict(xtest_buz)
 print(knn.score(xtest_buz,ytest_buz))
@@ -324,12 +341,3 @@ print('R2 Value:',r2_score(ytest_buz, knn_price_pred_econ))
 #%%
 
 # %%
-number_of_observations=50
-x_ax = range(len(ytest_econ[:number_of_observations]))
-plt.plot(x_ax, ytest_econ[:number_of_observations], label="original")
-plt.plot(x_ax, price_pred_econ[:number_of_observations], label="predicted")
-plt.title("Flight Price test and predicted data")
-plt.xlabel('Observation Number')
-plt.ylabel('Price')
-plt.legend()
-plt.show()
