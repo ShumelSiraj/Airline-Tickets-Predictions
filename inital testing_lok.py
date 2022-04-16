100209e62aed247b42d3fa7777c6d4de7aaffc71
LokeshBokkisam-data <bokkisamlokeshchowdary@gmail.com>
2022-04-16 12:39:42 -0400

    test

--- inital testing_lok.py
+++ inital testing_lok.py
@@ -52,7 +52,21 @@ buz_source=df_source[df_source['class']=='Business']
 
 
 #%% scatterplots for business class
-
+fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20))
+plt.xlim(0, 55)
+fig1.suptitle("Scatterplot for Economy Class", fontsize=60)
+ax1=sns.regplot( ax=ax1, x="duration", y="price", data=econ[econ['stops']==0],scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax1.set_title("0 stops", fontsize=30)
+ax1.set_ylim(1, 40000)
+ax2=sns.regplot( ax=ax2, x="duration", y="price", data=econ[econ['stops']==1],
+scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax2.set_title("1 stop", fontsize=30)
+ax2.set_ylim(1, 40000)
+ax3=sns.regplot( ax=ax3, x="duration", y="price", data=econ[econ['stops']==2],
+scatter_kws={"color": "black"}, line_kws={"color": "red"})
+ax3.set_title("2 or more stops", fontsize=30)
+ax3.set_ylim(1, 40000)
+#%%
 # Business 
 fig1, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(55,20))
 plt.xlim(0, 55)
@@ -83,5 +97,9 @@ plt.show()
 sns.barplot(x = 'days_left',
             y = 'price',
             data = df)
+plt.show()
+# %%
+sns.set()
+sns.catplot(y = "price", x = "airline", data = df, kind="boxen", height = 6, aspect = 3)
 plt.show()
 # %%
