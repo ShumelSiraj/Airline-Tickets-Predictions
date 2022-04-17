8cf49fe60307dcd02315596ccc7a77e68ff27dce
Shumelsiraj <shumelsiraj@gwu.edu>
2022-04-17 19:59:42 -0400

    Update Initial testing- Shumel.py

--- Initial testing- Shumel.py
+++ Initial testing- Shumel.py
@@ -1,5 +1,6 @@
 
 #%%
+from os import remove
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -93,9 +94,53 @@ print('Check for duplicate values')
 print(f"We have {df.duplicated().sum()} duplicate values")
 #
 #%%
-# Frequency histogram of all the variables:
-df.hist(layout=(5, 4), color='blue', figsize= (15, 12), grid=True)
-plt.suptitle("Histogram plots for all variables")
+# EDA
+print(df.shape)
+#%%
+print(df.info())
+#%%
+print(df.describe())
+#%%
+corr = df.corr()
+#%%
+corr.style.background_gradient(cmap='plasma')
+#%%
+#Now lets take a look at how the Ticekt Price is distributed
+print(df['Price'].describe())
+plt.figure(figsize=(9, 8))
+sns.distplot(df['Price'], color='g', bins=100, hist_kws={'alpha': 0.4});
+
+#%%
+#Now we'll try to find which features are strongly correlated with Price. We'll store them in a var called important_features_list. We'll reuse our df dataset to do so.
+df_corr = df.corr()['Price'][:-1] # -1 because the latest row is Price
+# df_corr value > 0.5
+important_feature_list = df_corr[abs(df_corr) > 0.5].sort_values(ascending=False)
+print("There is {} strongly correlated values greater than 0.5 with Price:\n{}".format(len(important_feature_list), important_feature_list))
+
+
+# df_corr value > 0.1
+important_feature_list = df_corr[abs(df_corr) > 0.1].sort_values(ascending=False)
+print("There is {} strongly correlated values greater than 0.1 with Price:\n{}".format(len(important_feature_list), important_feature_list))
+
+
+#With this information we can see that the prices are skewed right and some outliers lies above ~80000. We will eventually want to get rid of the them to get a normal distribution of the independent variable (`Price`) for machine learning.
+#%%
+#Numerical data distribution
+#For this part lets look at the distribution of all of the features by ploting them
+
+#To do so lets first list all the types of our data from our dataset and take only the numerical ones:
+list(set(df.dtypes.tolist()))
+df_num = df.select_dtypes(include = ['float64', 'int64'])
+df_num.head()
+
+df_num.hist(figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8);
+
+
+#%%
+from scipy.stats import shapiro
+data = df(columns= ['Source_City', 'Departure_Time', 'Stops', 'Arrival_Time', 'Destination_City', 'Class', 'Duration', 'Days_Left', 'Price']
+)
+stats, p = shapiro(data)
 #%%
 #Checking the normality
 #Shapiro-Wilk test: This test is most popular to test the normality. It has below hypothesis:
@@ -114,13 +159,38 @@ for i in columns:
 
 # %%
 #QQplot
-import pylab as py
-columns= ['Source_City', 'Departure_Time', 'Stops', 'Arrival_Time', 'Destination_City', 'Class', 'Duration', 'Days_Left', 'Price']
+import numpy as np
+import pylab
+import scipy.stats as stats
 
-for i in columns:
-  print([i])
-  qqplot= sm.qqplot(df[[i]], line ='45')
-  py.show()
+#qqdf = df[['Source_City', 'Departure_Time', 'Stops', 'Arrival_Time', 'Destination_City', 'Class', 'Duration', 'Days_Left', 'Price']]
+stats.probplot(df['Source_City'], dist='norm', plot=pylab)
+plt.title("Source City")
+pylab.show()
+stats.probplot(df['Destination_City'], dist='norm', plot=pylab)
+plt.title("Source City")
+pylab.show()
+stats.probplot(df['Arrival_Time'], dist='norm', plot=pylab)
+plt.title("Arrival Time")
+pylab.show()
+stats.probplot(df['Departure_Time'], dist='norm', plot=pylab)
+plt.title("Departure Time")
+pylab.show()
+stats.probplot(df['Stops'], dist='norm', plot=pylab)
+plt.title("Stops")
+pylab.show()
+stats.probplot(df['Class'], dist='norm', plot=pylab)
+plt.title("Class")
+pylab.show()
+stats.probplot(df['Duration'], dist='norm', plot=pylab)
+plt.title("Duration")
+pylab.show()
+stats.probplot(df['Days_Left'], dist='norm', plot=pylab)
+plt.title("Days left")
+pylab.show()
+stats.probplot(df['Price'], dist='norm', plot=pylab)
+plt.title("Price")
+pylab.show()
   
 #%%
 # Because the length of the data for the economy and business is the same, we will divide the data and analyze it separately.
